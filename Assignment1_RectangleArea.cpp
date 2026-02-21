c58261de0a78f06fe9c3155557acb0f0da4a7209
Kim, Lily <choiheewoo@gmail.com>
2026-02-21 15:17:37 -0800

    Code changes complete

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -4,7 +4,35 @@ using namespace std;
 
 int main()
 {
-	
+
+    double firstLength;
+    double firstWidth;
+    double secondLength;
+    double secondWidth;
+
+    cout << "Length of first rectangle: " << endl;
+    cin >> firstLength;
+    cout << "Width of the first rectangle: " << endl;
+    cin >> firstWidth;
+    cout << "Length of the second rectangle: " << endl;
+    cin >> secondLength;
+    cout << "Width of the second rectangle: " << endl;
+    cin >> secondWidth;
+
+    if ( (firstLength * firstWidth) > (secondLength * secondWidth) )
+    {
+        cout << "The 1st rectangle has the greater area" << endl;
+    }
+
+    else if ( (firstLength * firstWidth) < (secondLength * secondWidth) )
+    {
+        cout << "The 2nd rectangle has the greater area" << endl;
+    }
+
+    else
+    {
+        cout << "The areas are the same" << endl;
+    }
+
     return 0;
 }
-
