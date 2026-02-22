f3d58e9faac6c4d5a48bf0a231797795896fe3b8
Kim, Lily <choiheewoo@gmail.com>
2026-02-21 16:02:09 -0800

    Code changes complete

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
+
 int main()
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
+  int unitNumber;
+  double finalPrice;
 
-    //express pace in minutes and seconds
-  /* write your code here */
+  cout << "Enter the number of units: " << endl;
+  cin >> unitNumber;
 
-  //express your output in 1 decimal points
+  if ( unitNumber >= 10 && unitNumber <= 19 )
+  {
+    finalPrice = ( (unitNumber * 199) - ((unitNumber * 199) * .20) );
+  }
 
+  else if ( unitNumber >= 20 && unitNumber <= 40 )
+  {
+    finalPrice = ( (unitNumber * 199) - ((unitNumber * 199) * .30) );
+  }
+
+  else if ( unitNumber >= 50 && unitNumber <= 99 )
+  {
+    finalPrice = ( (unitNumber * 199) - ((unitNumber * 199) * .40) );
+  }
+
+  else if ( unitNumber >= 100 )
+  {
+    finalPrice = ( (unitNumber * 199) - ((unitNumber * 199) * .50) );
+  }
+
+  else
+  {
+    finalPrice = ( (unitNumber * 199) );
+  }
+
+  cout << fixed << setprecision(2);
+  cout << "The final price is: $" << finalPrice << endl;
+
+  return 0;
 
 }
